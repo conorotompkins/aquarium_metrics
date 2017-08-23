c8f63d9ae5ce481d8ce74909e98b9fae68027083
conorotompkins <conorotompkins@gmail.com>
2017-08-22 21:10:23 -0400

    created script

---
+++ aquarium_metrics.R
@@ -0,0 +1,56 @@
+install.packages("googlesheets")
+
+library(googlesheets)
+library(tidyverse)
+library(lubridate)
+library(ggrepel)
+
+theme_set(theme_bw())
+
+rm(list = ls())
+
+set.seed(1234)
+
+gs_ls()
+
+aquarium_metrics <- gs_title("Aquarium metrics")
+
+df_aquarium_metrics <- aquarium_metrics %>% 
+  gs_read(ws = 1)
+
+df_aquarium_metrics_recommended <- aquarium_metrics %>% 
+  gs_read(ws = 2)
+
+df_aquarium_metrics <- df_aquarium_metrics %>% 
+  left_join(df_aquarium_metrics_recommended) %>% 
+  mutate(date = mdy(date))
+
+df_aquarium_metrics_last <-  df_aquarium_metrics %>% 
+  group_by(metric) %>% 
+  summarize(measurement = last(measurement))
+
+df_aquarium_metrics %>% 
+  #group_by(metric) %>% 
+  #mutate(last_measurement = last(measurement)) %>% 
+  filter(before_after == "After") %>% 
+  ggplot(aes(metric, measurement)) +
+  geom_hline(aes(yintercept = min), color = "red") +
+  geom_hline(aes(yintercept = max), color = "red", linetype = 2) +
+  geom_jitter(alpha = .5,
+              width = .1) +
+  geom_text_repel(data = df_aquarium_metrics, aes(x = metric, y = measurement, label = measurement), segment.size = 5) +
+  facet_wrap(~metric, 
+             scales = "free")
+
+?geom_label_repel
+
+df_aquarium_metrics %>% 
+  filter(before_after == "After") %>% 
+  ggplot(aes(date, measurement, group = 1)) +
+  geom_hline(aes(yintercept = min), color = "red") +
+  geom_hline(aes(yintercept = max), color = "red", linetype = 2) +
+  geom_point() +
+  geom_line() +
+  facet_wrap(~metric,
+             scales = "free_y")
+  
