1be45c279b6f7aa19c02b873b90478cb58d015c9
Conor Tompkins <conorotompkins@gmail.com>
2017-11-26 17:51:06 -0500

    updated code for charts

--- scripts/aquarium_metrics.R
+++ scripts/aquarium_metrics.R
@@ -23,12 +23,10 @@ df_aquarium_metrics <- df_aquarium_metrics %>%
   left_join(df_aquarium_metrics_recommended) %>% 
   mutate(date = mdy(date))
 
-df_aquarium_metrics_last <-  df_aquarium_metrics %>% 
-  group_by(metric) %>% 
-  summarize(last_measurement = last(measurement))
-
 df_aquarium_metrics %>% 
-  left_join(df_aquarium_metrics_last) -> df_aquarium_metrics
+  group_by(metric) %>% 
+  summarize(last_date = last(date), 
+            last_measurement = last(measurement))-> df_aquarium_metrics_tag
 
 df_aquarium_metrics %>% 
   filter(before_after == "After") %>% 
@@ -36,11 +34,12 @@ df_aquarium_metrics %>%
   geom_hline(aes(yintercept = min), color = "red") +
   geom_hline(aes(yintercept = max), color = "red", linetype = 2) +
   geom_jitter(alpha = .5,
-              width = .1,
+              width = .05,
               height = 0) +
-  #geom_label_repel(aes(x = metric, y = last_measurement, label = last_measurement), segment.size = 5) +
+  geom_point(data = df_aquarium_metrics_tag, aes(x = metric, y = last_measurement), shape = 21, size = 3, fill = "red") +
   facet_wrap(~metric, 
-             scales = "free")
+             scales = "free",
+             nrow = 1)
 ggsave(paste0("images/Aquarium metrics summary ", Sys.Date(), ".png"))
 
 df_aquarium_metrics %>% 
@@ -51,5 +50,6 @@ df_aquarium_metrics %>%
   geom_point() +
   geom_line() +
   facet_wrap(~metric,
-             scales = "free_y")
+             scales = "free_y",
+             ncol = 1)
 ggsave(paste0("images/Aquarium metrics timeline ", Sys.Date(), ".png"))
